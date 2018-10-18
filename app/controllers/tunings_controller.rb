d3cfc00344f44711812cafccf85693db8117a2b3
CP92 <brd4jc@gmail.com>
2018-10-18 14:36:43 -0400

    Added frozen literal and i% to controller

--- app/controllers/tunings_controller.rb
+++ app/controllers/tunings_controller.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class TuningsController < ApplicationController
-  before_action :set_tuning, only: [:show, :update, :destroy]
+  before_action :set_tuning, only: %i[:show, :update, :destroy]
 
   # GET /tunings
   def index
