a7a55359e67c985298439e74e99770c7e27366ac
CP92 <brd4jc@gmail.com>
2018-10-23 11:45:07 -0400

    Fixed update and destroy bug

--- app/controllers/tunings_controller.rb
+++ app/controllers/tunings_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class TuningsController < OpenReadController
-  before_action :set_tuning, only: %i[:show, :update, :destroy]
+  before_action :set_tuning, only: %i[show update destroy]
 
   # GET /tunings
   def index
